57fb31b6cb07ced0be481fd781ae31cc5ac469a6
JoshyPHP <s9e.dev@gmail.com>
2018-04-21 20:09:13 +0200

    WiP

---
+++ tests/Configurator/RendererGenerators/PHP/XPathConvertor/Convertors/MultiByteStringManipulationTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace s9e\TextFormatter\Tests\Configurator\RendererGenerators\PHP\XPathConvertor\Convertors;
+
+/**
+* @covers s9e\TextFormatter\Configurator\RendererGenerators\PHP\XPathConvertor\Convertors\AbstractConvertor
+* @covers s9e\TextFormatter\Configurator\RendererGenerators\PHP\XPathConvertor\Convertors\MultiByteStringManipulation
+*/
+class MultiByteStringManipulationTest extends AbstractConvertorTest
+{
+	public function getConvertorTests()
+	{
+		return [
+			// Substring
+			[
+				"substring('\xC3\xB7\xC3\xB7\xC3\xB7', 2, 1)",
+				"mb_substr('\xC3\xB7\xC3\xB7\xC3\xB7',1,1,'utf-8')"
+			],
+		];
+	}
+}
