6f7cdb944633d8423167836fc3e2394163c6771e
JoshyPHP <s9e.dev@gmail.com>
2018-04-21 21:24:38 +0200

    WiP

---
+++ tests/Configurator/RendererGenerators/PHP/XPathConvertor/Convertors/SingleByteStringFunctionsTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace s9e\TextFormatter\Tests\Configurator\RendererGenerators\PHP\XPathConvertor\Convertors;
+
+/**
+* @covers s9e\TextFormatter\Configurator\RendererGenerators\PHP\XPathConvertor\Convertors\AbstractConvertor
+* @covers s9e\TextFormatter\Configurator\RendererGenerators\PHP\XPathConvertor\Convertors\SingleByteStringFunctions
+*/
+class SingleByteStringFunctionsTest extends AbstractConvertorTest
+{
+	public function getConvertorTests()
+	{
+		return [
+			// Contains
+			[
+				'contains(@foo, @bar)',
+				"(strpos(\$node->getAttribute('foo'),\$node->getAttribute('bar'))!==false)"
+			],
+			// NotContains
+			[
+				'not(contains(@foo, @bar))',
+				"(strpos(\$node->getAttribute('foo'),\$node->getAttribute('bar'))===false)"
+			],
+			// NotStartsWith
+			[
+				'not(starts-with(@foo, @bar))',
+				"(strpos(\$node->getAttribute('foo'),\$node->getAttribute('bar'))!==0)"
+			],
+			// StartsWith
+			[
+				'starts-with(@foo, @bar)',
+				"(strpos(\$node->getAttribute('foo'),\$node->getAttribute('bar'))===0)"
+			],
+			// StringLength
+			[
+				'string-length(@foo)',
+				"preg_match_all('(.)su',\$node->getAttribute('foo'))"
+			],
+		];
+	}
+}
